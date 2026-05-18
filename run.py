b8fa51f36365af7ea0b932f91f28c4539f739843
faraaaa <farahrahin25@gmail.com>
2026-05-18 20:59:53 +0800

    run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-# run.py
 import os
 import uvicorn
 
-port = int(os.environ.get("PORT",8000))
-uvicorn.run("main_v5:app", host="0.0.0.0", port=port)
+port = int(os.environ.get("PORT", 8000))
+
+uvicorn.run(
+    "main_v5:app",
+    host="0.0.0.0",
+    port=port
+)
