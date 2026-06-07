3aba4aac703f30f81e2c3b1cd31d60f9f83fbd7f
faraaaa <farahrahin25@gmail.com>
2026-06-08 07:54:57 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,10 +1,4 @@
-import os
 import uvicorn
 
-port = int(os.environ.get("PORT", 8000))
-
-uvicorn.run(
-    "main_v5:app",
-    host="0.0.0.0",
-    port=port
-)
+if __name__ == "__main__":
+    uvicorn.run("main_v5:app", host="0.0.0.0", port=int(__import__("os").environ.get("PORT", 8000)))
